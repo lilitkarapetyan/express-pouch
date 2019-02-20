12100ffcf2250e01287bc7162715af1ae099a707
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-02-20 02:07:25 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var PouchDB = require('pouchdb');
+var express = require('express');
+var cors = require('cors')
+var app = express();
+
+app.use(cors());
+
+app.use('/', require('express-pouchdb')(PouchDB));
+
+app.listen(3200);

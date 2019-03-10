42c7f47a11a5aa400e925aaa7a4289dfa3953500
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-03-10 13:07:42 -0700

    port

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(cors());
 
 app.use('/', require('express-pouchdb')(PouchDB));
 
-app.listen(80);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port);
